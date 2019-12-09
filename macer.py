06657abe6d31567c6ca012f3a6f17ec55079d868
262315228@qq.com <262315228@qq.com>
2019-12-09 04:42:55 -0800

    regularize

--- macer.py
+++ macer.py
@@ -68,17 +68,22 @@ def macer_train(method, sigma_net, logsub, lbd, gauss_num, beta, gamma, lr_sigma
             out0, out1 = top2_score[indices_correct, 0], top2_score[indices_correct, 1]
 
             if logsub is 'True':
-                robustness_loss = out0 * torch.log(out1 + 1.0)
-                robustness_loss = (robustness_loss * sigma_this_batch[indices_correct]).sum() / \
-                                  robustness_loss.size()[0].item() * batch_size
+                robustness_loss = out0 * torch.log(out1)
             else:
                 robustness_loss = m.icdf(out1) - m.icdf(out0)
-                indices = ~torch.isnan(robustness_loss) & ~torch.isinf(
+
+            indices = ~torch.isnan(robustness_loss) & ~torch.isinf(
                     robustness_loss) & (torch.abs(robustness_loss) <= gamma)  # hinge
-                indices_correct = utils.cal_index(indices_correct, indices)
-                out0, out1 = out0[indices], out1[indices]
+            indices_correct = utils.cal_index(indices_correct, indices)
+            out0, out1 = out0[indices], out1[indices]
+
+            if logsub is 'True':
+                robustness_loss = gamma - out0 * torch.log(out1)
+            else:
                 robustness_loss = m.icdf(out1) - m.icdf(out0) + gamma
-                robustness_loss = (robustness_loss * sigma_this_batch[indices_correct]).sum() / 2
+
+            robustness_loss = (robustness_loss * sigma_this_batch[indices_correct]).sum() / \
+                                  robustness_loss.size()[0].item() * batch_size
 
             rl_total += robustness_loss.item()
 
